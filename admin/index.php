a7db1ad11a0963a91461407d5a2ef5d0bee6c8df
Xian Shi <xianshi915@gmail.com>
2021-06-23 12:55:37 -0400

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+        <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                       
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+<?php include "includes/admin_footer.php"; ?>
---
+++ includes/db.php
@@ -0,0 +1,8 @@
+<?php
+
+$connection = mysqli_connect("localhost", "root", "root", "cms");
+
+// if($connection){
+//     echo "we are connected";
+// } 
+?>
